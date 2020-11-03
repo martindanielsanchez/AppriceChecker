7224a3acc3153a6bc9d58e0bc384770fc728fe4f
Martin Sanchez <martinsanche2016@fau.edu>
2020-11-03 14:22:32 -0500

    Add project files.

---
+++ AppriceChecker/AppriceChecker/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppriceChecker
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Handle_Clicked_Search(object sender, System.EventArgs e)
+        {
+            //add code to call Scan page
+           var page = new ScanProduct();
+           Navigation.PushAsync(page);
+        }
+    }
+}
---
+++ AppriceChecker/AppriceChecker/Registration.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppriceChecker
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registration : ContentPage
+    {
+        public Registration(String result)
+        {
+            InitializeComponent();
+            resultScan.Text = result;
+        }
+    }
+}
