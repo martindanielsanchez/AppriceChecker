b1194bcf7342f8fe3ffeb2cda97f671bd99cdfa9
Martin Sanchez <martinsanche2016@fau.edu>
2020-11-19 15:59:36 -0500

    Update Products in Database

--- AppriceChecker/AppriceChecker/ProductViewPage.xaml.cs
+++ AppriceChecker/AppriceChecker/ProductViewPage.xaml.cs
@@ -15,14 +15,18 @@ namespace AppriceChecker
     {
         RestService _restService;
         String barcode;
+        String name;
+        String price;
+        String location;
         public ProductViewPage(ItemData item)
         {
             InitializeComponent();
             barcode = item.ProductId.ToString();
-            itemBarcode.Text = "Barcode: " + item.ProductId.ToString();
-            itemName.Text = "Name: " + item.Name;
-            itemPrice.Text = "Unit Price: $" + item.UnitPrice;
-            itemLocation.Text = "Location: " + item.Location;
+            
+            itemBarcode.Text = item.ProductId.ToString();
+            itemName.Text = item.Name;
+            itemPrice.Text = item.UnitPrice.ToString();
+            itemLocation.Text = item.Location;
         }
 
         void DeleteClicked(object sender, System.EventArgs e)
@@ -39,10 +43,36 @@ namespace AppriceChecker
             Navigation.PushAsync(page);
         }
 
+        void UpdateClicked(object sender, System.EventArgs e)
+        {
+
+            name = itemName.Text;
+            price = itemPrice.Text;
+            location = itemLocation.Text;
+
+            _restService = new RestService();
+
+            //API call
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                String message = await _restService.GetItemAdditionAsync(GenerateUpdateRequestUri(App.Global._APIendpoint));
+            });
+
+            var page = new RetailPortal();
+            Navigation.PushAsync(page);
+        }
+
         string GenerateRequestUri(string endpoint)
         {
             string requestUri = endpoint;
             requestUri += $"/DeleteProduct?barcode={barcode}";
+            return requestUri;
+        }
+
+        string GenerateUpdateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"/UpdateProduct?barcode={barcode}&name={name}&location={location}&price={price}";
             return requestUri;
         }
     }
